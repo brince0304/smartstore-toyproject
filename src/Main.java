6c0f4d6c9386f18ec3cf58db5ef27ed37df5246b
brince0304 <brince0304@gmail.com>
2022-11-30 17:02:19 +0900

    완료

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        SmartStoreApplication.getInstance().test();
+        //SmartStoreApplication.getInstance().test();
             SmartStoreApplication.getInstance().run();
     }
 }
